9c76506df8471ab3df1542255f1789b5640603f3
FuzenPuzen <angel017791@gmail.com>
2023-12-11 17:38:12 +0300

    O tuun

---
+++ Assets/Scripts/Data/PlayerSuperJumpBehaviourSODatas.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerSuperJumpBehaviourSODatas", menuName = "PlayerSuperJumpBehaviourSODatas")]
+public class PlayerSuperJumpBehaviourSODatas :ScriptableObject
+{
+    public List<PlayerSuperJumpBehaviourSOData> playerSuperJumpBehaviourSODatas;
+}
