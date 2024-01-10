01b3301d98fef455fbb2d3020e17d060143d1427
DevKevINTender <tenderkevin.dev@gmail.com>
2024-01-10 14:52:15 +0300

    Add CoinPalleteView coinCollect logicj from CoinDataManager

--- Assets/Scripts/View/Pallete/CoinPalleteView.cs
+++ Assets/Scripts/View/Pallete/CoinPalleteView.cs
@@ -1,15 +1,22 @@
 using Zenject;
 using UnityEngine;
+using System;
 
 public class CoinPalleteView : MonoBehaviour
 {
+	private Action _cointCollected;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<GiftCoinView>(out GiftCoinView component))
         {
-			// Добавить монетки
-			print("Пришла бонусная монетка");
+			_cointCollected?.Invoke();
         }
+    }
+
+    public void SetActionCoinCollecte(Action cointCollected)
+    {
+        _cointCollected = cointCollected;
     }
 }
 
@@ -18,19 +25,28 @@ public class CoinPalleteViewService : IService
 	private CoinPalleteView _coinPalleteView;
 	private IViewFabric _fabric;
 	private IMarkerService _markerService;
+	private ICoinDataManager _coinDataManager;
 	[Inject]
 	public void Constructor(
 		IViewFabric fabric,
-		IMarkerService markerService)
+		IMarkerService markerService,
+		ICoinDataManager coinDataManager)
 	{
 		_fabric = fabric;
 		_markerService = markerService;
+		_coinDataManager = coinDataManager;
 	}
 
 	public void ActivateService()
 	{
 		Transform target = _markerService.GetTransformMarker<CoinPalleteSpawnPosMarker>().transform;
         _coinPalleteView = _fabric.SpawnObject<CoinPalleteView>(target);
+		_coinPalleteView.SetActionCoinCollecte(OnCoinCollected);
 
     }
+
+	private void OnCoinCollected()
+	{
+		_coinDataManager.CollectCoins();
+	}
 }
