4894968515f8b7fb2e8e26e5ab2bf06caf0e9695
FuzenPuzen <angel017791@gmail.com>
2024-02-21 10:55:11 +0300

    Add custom Editor Setting to Shake Anim, Add shake to main menu buttons

--- Assets/Scripts/SupportServices/Animations/SimpleAnimation/ShakeAnim.cs
+++ Assets/Scripts/SupportServices/Animations/SimpleAnimation/ShakeAnim.cs
@@ -1,8 +1,8 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using Sirenix.OdinInspector;
+using System;
 
 [ShowOdinSerializedPropertiesInInspector]
 public class ShakeAnim : Anim
@@ -11,9 +11,51 @@ public class ShakeAnim : Anim
     [SerializeField] private float _shakeForce;
     [SerializeField] private int _shakeVibrato;
     [SerializeField] private bool _loop;
+    [ShowIf("_loop")]
+    [SerializeField] private float _loopDelay;
     [SerializeField] private bool _autoPlay;
     [ShowIf("_autoPlay")]
     [SerializeField] private float _startDelay;
+
+
+    private void Start()
+    {
+        StartAnimation();
+    }
+
+    private void OnEnable()
+    {
+        StartAnimation();
+    }
+
+    private void StartAnimation()
+    {
+        if (!_autoPlay) return;
+        if (_loop)
+        {
+            StartCoroutine(StartDelay(_startDelay, PlayLooped)); return;
+        }
+        StartCoroutine(StartDelay(_startDelay, Play));
+    }
+
+    public void PlayLooped()
+    {
+        _animSequence = DOTween.Sequence();
+        _animSequence.Append(transform.DOShakePosition(_shakeDuration, _shakeForce, _shakeVibrato));
+        StartCoroutine(ShakeDelay(_loopDelay));
+    }
+
+    private IEnumerator StartDelay(float delay, Action action)
+    {
+        yield return new WaitForSecondsRealtime(delay);
+        action?.Invoke();
+    }
+
+    private IEnumerator ShakeDelay(float delay)
+    {
+        yield return new WaitForSecondsRealtime(delay);
+        PlayLooped();
+    }
 
     public override void Play()
     {
@@ -47,7 +89,7 @@ public class ShakeAnimData : AnimData
 
 public abstract class Anim : MonoBehaviour
 {
-    public Sequence _animSequence;
+    internal Sequence _animSequence;
     public abstract void Play();
     public abstract void Stop();
 
